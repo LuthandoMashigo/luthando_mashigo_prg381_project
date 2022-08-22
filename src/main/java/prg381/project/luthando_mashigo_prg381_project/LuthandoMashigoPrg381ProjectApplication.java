b704c69098ac5a5478cc49798a56c9b3f98b76ed
Luthando Mashigo <576765@student.belgiumcampus.ac.za>
2022-08-22 20:40:28 +0200

    first commit

---
+++ src/main/java/prg381/project/luthando_mashigo_prg381_project/LuthandoMashigoPrg381ProjectApplication.java
@@ -0,0 +1,13 @@
+package prg381.project.luthando_mashigo_prg381_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuthandoMashigoPrg381ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuthandoMashigoPrg381ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prg381/project/luthando_mashigo_prg381_project/LuthandoMashigoPrg381ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package prg381.project.luthando_mashigo_prg381_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuthandoMashigoPrg381ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
