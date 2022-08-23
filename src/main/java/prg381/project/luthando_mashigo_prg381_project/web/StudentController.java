80faea2eb6688efd001f980cbed3c02ab66d2137
Luthando Mashigo <576765@student.belgiumcampus.ac.za>
2022-08-23 10:10:01 +0200

    Configuration admin page for view students , CRUD

---
+++ src/main/java/prg381/project/luthando_mashigo_prg381_project/web/StudentController.java
@@ -0,0 +1,74 @@
+package prg381.project.luthando_mashigo_prg381_project.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import prg381.project.luthando_mashigo_prg381_project.model.Student;
+import prg381.project.luthando_mashigo_prg381_project.service.StudentService;
+
+@Controller
+public class StudentController {
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
+	}
+	
+	@GetMapping("/students/new")
+	public String createStudentForm(Model model) {
+		
+		// create student object to hold student form data
+		Student student = new Student();
+		model.addAttribute("student", student);
+		return "StudentRegistration";
+		
+	}
+	
+	@PostMapping("/students")
+	public String saveStudent(@ModelAttribute("student") Student student) {
+		studentService.saveStudent(student);
+		return "redirect:/students";
+	}
+	
+	@GetMapping("/students/edit/{id}")
+	public String editStudentForm(@PathVariable Long id, Model model) {
+		model.addAttribute("student", studentService.getStudentById(id));
+		return "UpdateStudent";
+	}
+
+	@PostMapping("/students/{id}")
+	public String updateStudent(@PathVariable Long id,
+			@ModelAttribute("student") Student student,
+			Model model) {
+		
+		// get student from database by id
+		Student existingStudent = studentService.getStudentById(id);
+		existingStudent.setId(id);
+		existingStudent.setStudent_name(student.getStudent_name());
+		existingStudent.setStudent_address(student.getStudent_address());
+		existingStudent.setEmail(student.getEmail());
+		existingStudent.setPassword(student.getPassword());
+		
+		// save updated student object
+		studentService.updateStudent(existingStudent);
+		return "redirect:/students";		
+	}
+	
+	// handler method to handle delete student request
+	
+	@GetMapping("/students/{id}")
+	public String deleteStudent(@PathVariable Long id) {
+		studentService.deleteStudentById(id);
+		return "redirect:/students";
+	}
+}
